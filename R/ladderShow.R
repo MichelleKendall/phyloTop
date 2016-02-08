119226680ddc702bea6b759c2a6d2c30b8b8de1a
Michelle Kendall <mlkendal@ma-mlkendal-1.ic.ac.uk>
2016-02-08 17:12:22 +0000

    Updating ladderShow to use fast ladder finding

--- R/ladderShow.R
+++ R/ladderShow.R
@@ -2,31 +2,31 @@
 #' 
 #' Plot a tree, highlighting any ladders within it
 #' 
-#' @author Michael Boyd \email{mboyd855@gmail.com}
 #' @author Michelle Kendall \email{michelle.louise.kendall@@gmail.com}
+#' @author Michael Boyd \email{mboyd855@gmail.com}
 #'   
 #' @param tree a tree of class \code{phylo} or \code{phylo4}
-#' @return A plot of the tree, with ladders highlighted in red.
+#' @param ladderCol colour with which to highlight ladder branches (default is red)
+#' @param ... further arguments to be passed to plot.phylo
+#' @return A plot of the tree, with ladders highlighted by colour.
 #'   
 #' @import ape
-#' @importFrom phylobase nodeId
-#' @importFrom phylobase nodeType
 #' @examples
 #' \dontrun{
-#' ladderShow(rtree(100))
+#' ladderShow(rtree(50), ladderCol="blue", edge.width=2)
 #' }
 #' 
 #' @export
-ladderShow <- function(tree) {
-  tree <- as(tree, "phylo4")
-  ladderNumbers <- ladderNums(tree)
-  col <- rep('black',nEdges(tree))
+ladderShow <- function(tree, ladderCol="red", ...) {
+  edgeList <- tree$edge
+  nEdges <- length(tree$edge[,1])
+  ladderBranches <- ladderSizes(tree)$ladderBr
+  col <- rep('black',nEdges)
   # If the ladder number is at least one make the edge red
-  E <- edges(tree)
-  for (i in 2:nEdges(tree)) {
-    if (ladderNumbers[E[i,1]] > 0) {col[i] <- 'red'}
+  for (i in ladderBranches) {
+    col[i] <- ladderCol
   }
-  plot(tree,edge.color=col)
+  plot(tree,edge.color=col, ...)
 }
 
 
