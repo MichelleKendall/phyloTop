da6d3bcfda2059462bf5a34a33759995c55a6545
Michelle Kendall <m.kendall@imperial.ac.uk>
2016-02-08 22:16:11 +0000

    Cataloguing deprecated functions

---
+++ R/deprecated.R
@@ -0,0 +1,111 @@
+baseAnalysis <- function(...) {
+  .Deprecated("phyloTop", package="phyloTop")
+}
+
+baseCreate <- function(...) {
+  .Deprecated("phyloTop", package="phyloTop")
+}
+
+baseListAnalysis <- function(...) {
+  .Deprecated("phyloTop", package="phyloTop")
+}
+
+baseListCreate <- function(...) {
+  .Deprecated("phyloTop", package="phyloTop")
+}
+
+classifyConfigFive <- function(...) {
+  .Deprecated("nConfig", package="phyloTop")
+}
+
+classifyConfigFour <- function(...) {
+  .Deprecated("nConfig", package="phyloTop")
+}
+
+configFind <- function(...) {
+  .Deprecated("nConfig", package="phyloTop")
+}
+
+configTop <- function(...) {
+  .Deprecated("nConfig", package="phyloTop")
+}
+
+dists <- function(...) {
+  .Deprecated("getDepths", package="phyloTop")
+}
+
+fAvgLadder <- function(...) {
+  .Deprecated("avgLadder", package="phyloTop")
+}
+
+fColless <- function(...) {
+  .Deprecated("colless.phylo", package="phyloTop")
+}
+
+fNConfig <- function(...) {
+  .Deprecated("nConfig", package="phyloTop")
+}
+
+fNLadders <- function(...) {
+  .Deprecated("ladderSizes", package="phyloTop")
+}
+
+fWidths <- function(...) {
+  .Deprecated("widths", package="phyloTop")
+}
+
+idNodeLabel <- function(...) {
+  .Deprecated("phyloTop", package="phyloTop", 
+              "Function deprecated: no longer needed now that functions use phylo objects.")
+}
+
+ladderDist <- function(...) {
+  .Deprecated("ladderSizes", package="phyloTop")
+}
+
+ladderNums <- function(...) {
+  .Deprecated("ladderSizes", package="phyloTop")
+}
+
+laddItr <- function(...) {
+  .Deprecated("ladderSizes", package="phyloTop")
+}
+
+nDescendants <- function(...) {
+  .Deprecated("phyloTop", package="phyloTop",
+              "Function deprecated: number of descendants is handled within the relevant functions.")
+}
+
+nLadders <- function(...) {
+  .Deprecated("ladderSizes", package="phyloTop")
+}
+
+nodeApply <- function(...) {
+  .Deprecated("phyloTop", package="phyloTop",
+              "Function deprecated: no longer needed now that functions use phylo objects.")
+}
+
+nTipChildren <- function(...) {
+  .Deprecated("nConfig", package="phyloTop")
+}
+
+nTipDescendants <- function(...) {
+  .Deprecated("phyloTop", package="phyloTop",
+              "Function deprecated: number of descendants is handled within the relevant functions.")
+}
+
+rootLaddDist <- function(...) {
+  .Deprecated("ladderSizes", package="phyloTop")
+}
+
+subtreeShow <- function(...) {
+  .Deprecated("configShow", package="phyloTop")
+}
+
+topSumm <- function(...) {
+  .Deprecated("phyloTop", package="phyloTop")
+}
+
+treeListSummary <- function(...) {
+  .Deprecated("phyloTop", package="phyloTop")
+}
