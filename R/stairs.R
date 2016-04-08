2af3c57dace2001ae5d41e2d17ff3f26eca212cb
Michelle Kendall <m.kendall@imperial.ac.uk>
2016-04-08 13:32:27 +0100

    Updating doi link to proper format

--- R/stairs.R
+++ R/stairs.R
@@ -6,7 +6,7 @@
 #' @author Michelle Kendall \email{michelle.louise.kendall@@gmail.com}
 #'   
 #' @param tree a tree of class \code{phylo} or \code{phylo4}. The tree should be binary and rooted; if not it will be coerced into a binary rooted tree using multi2di, if possible.
-#' @return Two numbers corresponding to the two staircase-ness measures for a tree. Defined in Norstrom et al., Evolutionary Bioinformatics online, 8:261 (2012) \url{doi::10.4137/EBO.S9738}, these are two related measures:
+#' @return Two numbers corresponding to the two staircase-ness measures for a tree. Defined in Norstrom et al., Evolutionary Bioinformatics online, 8:261 (2012) \doi{10.4137/EBO.S9738}, these are two related measures:
 #' \itemize{ 
 #' \item 1: the proportion of subtrees that are imbalanced (i.e. subtrees where the left child has more tip descendants than the right child, or vice versa)
 #' \item 2: the average of all the min(l,r)/max(l,r) values of each subtree, where l and r are the number of tips in the left and right children of a subtree.
